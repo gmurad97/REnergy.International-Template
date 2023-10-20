a1bef77e3b552f953ae87e1718e2ee63de51fe31
gmurad97 <murad.dev@bk.ru>
2023-10-20 21:00:49 +0400

    release

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,88 @@
-/* SCROLL 100VH BUTTON - START */
-const mainScrollBtn = document.querySelector("#description_block_main_scroll_btn");
-
-if (mainScrollBtn) {
-    mainScrollBtn.addEventListener("click", function () {
-        window.scroll(0, window.innerHeight);
+$(document).ready(function () {
+    /* MAIN SCROLL BUTTON - START */
+    const mainScrollBtn = $("#description_block_main_scroll_btn");
+    mainScrollBtn.on("click", function () {
+        window.scrollTo(0, window.innerHeight);
     });
-}
-/* SCROLL 100VH BUTTON - ENDED */
+    /* MAIN SCROLL BUTTON - ENDED */
 
-/* SERVICES ANIME.JS - START */
-const serviceBlocks = document.querySelectorAll(".services__block");
+    /* SERVICES ANIMATION ANIME.JS - START */
+    $(".services__block").each(function () {
+        const circleService = $(this).find(".services__icon-block--circle");
 
-serviceBlocks.forEach(function (serviceBlock) {
-    const circleService = serviceBlock.querySelector(".services__icon-block--circle");
+        $(this).on("mouseenter", function () {
+            anime.remove(circleService[0]);
+            anime({
+                targets: circleService[0],
+                scale: 8,
+                easing: "linear",
+                duration: 256
+            });
+        });
 
-    serviceBlock.addEventListener("mouseenter", function () {
-        anime.remove(circleService);
-        anime({
-            targets: circleService,
-            scale: 7,
-            easing: "linear", // Используйте линейное замедление, чтобы избежать "прыгучести"
-            duration: 256
+        $(this).on("mouseleave", function () {
+            anime.remove(circleService[0]);
+            anime({
+                targets: circleService[0],
+                scale: 1,
+                easing: 'spring(1, 50, 10, 0)'
+            });
         });
     });
+    /* SERVICES ANIMATION ANIME.JS - ENDED */
 
-    serviceBlock.addEventListener("mouseleave", function () {
-        anime.remove(circleService);
-        anime({
-            targets: circleService,
-            scale: 1,
-            easing: 'spring(1, 50, 9, 0)'
-        });
+    /* FOOTER PARTNERS OWL CAROUSEL - START */
+    $(document).ready(function () {
+        if ($('#owl-carousel-partners').length) {
+            $('#owl-carousel-partners').owlCarousel({
+                items: 8,
+                loop: true,
+                center: false,
+                nav: false,
+                dots: false
+            });
+        }
     });
+    /* FOOTER PARTNERS OWL CAROUSEL - ENDED */
 });
-/* SERVICES ANIME.JS - ENDED */
+
+
+
+
+let miniblock = document.querySelector(".faq__minimazed-detail");
+let maxiblock = document.querySelector(".faq__maximized-detail");
+
+function on(ret){
+    if(ret){
+        miniblock.style.transform = "translate(0px, 50px)";
+        miniblock.style.opacity = "0";
+    
+        maxiblock.style.transform = "translate(0px, -50px)";
+        maxiblock.style.opacity = "1";
+        maxiblock.style.maxHeight = "384px";
+
+
+
+
+    }
+    else{
+        miniblock.style.transform = "translate(0px, 0px)";
+        miniblock.style.opacity = "1";
+    
+    
+        maxiblock.style.transform = "translate(0px, 0px)";
+        maxiblock.style.opacity = "0";
+        maxiblock.style.maxHeight = "0px";
+    }
+}
+
+
+document.querySelector("#maximized_detail").addEventListener("click", function () {
+    on(true);
+});
+
+document.querySelector("#minimazed_detail").addEventListener("click", function () {
+    on(false);
+
+});
+
